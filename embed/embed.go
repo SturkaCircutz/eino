b483759c4871c9a2b8fdee483555c30a62123f8f
SturkaCircutz <313721325sjw@gmail.com>
2026-09-02 16:45:42 -0600

    new

---
+++ embed/embed.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/cloudwego/eino-ext/components/embedding/openai"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+	ctx := context.Background()
+	embedder, err := openai.NewEmbedder(ctx, &openai.EmbeddingConfig{
+		APIKey:  os.Getenv("COHERE_API_KEY"),
+		BaseURL: os.Getenv("COHERE_BASE_URL"),
+		Model:   os.Getenv("COHERE_EMBED_MODEL"),
+	})
+	if err != nil {
+		panic(err)
+	}
+	texts := []string{
+		"my name is John",
+		"im 13 years old",
+	}
+	embeddings, err := embedder.EmbedStrings(ctx, texts)
+	if err != nil {
+		panic(err)
+	}
+	for i, embedding := range embeddings {
+		fmt.Printf("Text %d: %s\nEmbedding: %d dims, first 5: %v\n", i+1, texts[i], len(embedding), embedding[:5])
+	}
+}
---
+++ flow/flow.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"os"
+
+	"github.com/cloudwego/eino-ext/components/model/openai"
+	"github.com/cloudwego/eino/schema"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+	ctx := context.Background()
+	model, err := openai.NewChatModel(ctx, &openai.ChatModelConfig{
+		BaseURL: os.Getenv("COHERE_BASE_URL"),
+		APIKey:  os.Getenv("COHERE_API_KEY"),
+		Model:   os.Getenv("COHERE_MODEL"),
+	})
+	if err != nil {
+		panic(err)
+	}
+	messages := []*schema.Message{
+		schema.SystemMessage("you are a good human helper"),
+		schema.UserMessage("which year is it"),
+	}
+
+	reader, err := model.Generate(ctx, messages)
+	if err != nil {
+		panic(err)
+
+	}
+	println(reader.Content)
+}
